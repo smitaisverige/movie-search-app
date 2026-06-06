b99285e79c253f7bb9dd26b5cf7117ffc7c929e4
Smita Mithun Redkar <smitaisverige@gmail.com>
2026-06-06 20:06:23 +0200

    changed default bg img

--- src/App.tsx
+++ src/App.tsx
@@ -11,45 +11,40 @@ const App = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
 
   const fetchMovies = async () => {
+    if (search.trim() === "") return;
 
-  if (search.trim() === "") return;
+    const API_KEY = import.meta.env.VITE_OMDB_KEY;
 
-  const API_KEY = import.meta.env.VITE_OMDB_KEY;
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${encodeURIComponent(search)}`,
+    );
 
-  const response = await fetch(
-    `https://www.omdbapi.com/?apikey=${API_KEY}&s=${encodeURIComponent(search)}`
-  );
+    const data = await response.json();
 
-  const data = await response.json();
+    setMovies(data.Search || []);
+  };
 
-  setMovies(data.Search || []);
-};
+  return (
+    <div className="min-h-screen flex flex-col bg-cover bg-right bg-no-repeat bg-[url('/background.jpg')]">
+      <div className="flex-grow">
+        <div className="max-w-7xl mx-auto p-6">
+          <h1 className="text-white text-4xl md:text-5xl font-bold text-center mb-8">
+            Movie Search App
+          </h1>
 
-return (
-<div className="min-h-screen flex flex-col bg-cover bg-center bg-no-repeat bg-[url('/background.jpg')]">
-  
-  <div className="flex-grow">
-    
-    <div className="max-w-7xl mx-auto p-6">
-      
-      <h1 className="text-white text-4xl md:text-5xl font-bold text-center mb-8">
-        Movie Search App
-      </h1>
+          <SearchBar
+            search={search}
+            setSearch={setSearch}
+            fetchMovies={fetchMovies}
+          />
 
-      <SearchBar
-        search={search}
-        setSearch={setSearch}
-        fetchMovies={fetchMovies}
-      />
+          <MovieList movies={movies} />
+        </div>
+      </div>
 
-      <MovieList movies={movies} />
-
+      <Footer />
     </div>
-  </div>
-
-  <Footer />
-</div>
-);
+  );
 };
 
 export default App;
--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -8,19 +8,17 @@ const MovieCard = ({ movie }: MovieCardProps) => {
   return (
     <div className="bg-white rounded-xl shadow-lg overflow-hidden hover:scale-105 transition">
       <img
-        src={
-          movie.Poster !== "N/A"
-            ? movie.Poster
-            : "https://picsum.photos/id/237/200/300"
-        }
+        src={movie.Poster !== "N/A" ? movie.Poster : "/default.jpg"}
         alt={movie.Title}
         className="w-full h-[400px] object-cover"
+        onError={({ currentTarget }) => {
+          currentTarget.onerror = null;
+          currentTarget.src = "/default.jpg";
+        }}
       />
 
       <div className="p-4">
-        <h2 className="text-lg font-bold mb-2">
-          {movie.Title}
-        </h2>
+        <h2 className="text-lg font-bold mb-2">{movie.Title}</h2>
 
         <p className="text-gray-600">{movie.Year}</p>
       </div>
